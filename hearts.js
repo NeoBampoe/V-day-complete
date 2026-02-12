23c02c7f32350ebd2cf7676d7d816c0e942afa75
NeoBampoe <neobampoe123@gmail.com>
2026-02-12 11:30:31 +0200

    Update hearts.js

--- hearts.js
+++ hearts.js
@@ -110,7 +110,7 @@ function createHeart() {
     }, 300);
 
     score++;
-    fallSpeed += 1.5;
+    fallSpeed += 1s;
     spawnRate = Math.max(200, spawnRate - 10);
 
     scoreText.textContent = `Score: ${score} | Lives: ${lives}`;
@@ -151,6 +151,7 @@ function loseLife() {
 
 // Start game
 spawnInterval = setInterval(createHeart, spawnRate);
+
 
 
 
